596f9571f0e615317364dc54cbc9eac79af6ae79
Muhammad Mubashir Munir Khan <100374421+mubashir05-beep@users.noreply.github.com>
2023-11-27 12:00:00 -0500

    Update

--- components/CenterContent/page.tsx
+++ components/CenterContent/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect } from "react";
+import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setOption } from "@/slices/counterSlice";
 import Link from "next/link";
