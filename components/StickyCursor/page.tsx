4af9b9061370b9caf0c9a4244a3e5a9bca90b256
Muhammad Mubashir Munir Khan <100374421+mubashir05-beep@users.noreply.github.com>
2023-12-08 03:36:02 -0500

    Update page.tsx

--- components/StickyCursor/page.tsx
+++ components/StickyCursor/page.tsx
@@ -1,35 +1,43 @@
-'use client';
-import { useEffect } from 'react';
+'use client'
+// Import necessary dependencies
+import React, { useEffect } from 'react';
 import styles from './style.module.scss';
 import { motion, useMotionValue, useSpring } from 'framer-motion';
 
-export default function index({stickyElement}:any) {
-
+// Define your component function with an uppercase letter
+const Index = ({ stickyElement }: any) => {
   const cursorSize = 15;
+
+  // Use React hooks inside the component
   const mouse = {
     x: useMotionValue(0),
     y: useMotionValue(0)
-  }
+  };
 
-  const smoothOptions = {damping: 20, stiffness: 300, mass: 0.5}
+  const smoothOptions = { damping: 20, stiffness: 300, mass: 0.5 };
   const smoothMouse = {
     x: useSpring(mouse.x, smoothOptions),
     y: useSpring(mouse.y, smoothOptions)
-  }
+  };
 
-  const manageMouseMove = (e: { clientX: any; clientY: any; }) => {
+  // Define the function inside the component
+  const manageMouseMove = (e: { clientX: any; clientY: any }) => {
     const { clientX, clientY } = e;
     mouse.x.set(clientX - cursorSize / 2);
     mouse.y.set(clientY - cursorSize / 2);
-  }
+  };
 
-  useEffect( () => {
-    window.addEventListener("mousemove", manageMouseMove);
+  // Use useEffect inside the component
+  useEffect(() => {
+    window.addEventListener('mousemove', manageMouseMove);
+
+    // Cleanup function
     return () => {
-      window.removeEventListener("mousemove", manageMouseMove)
-    }
-  }, [])
+      window.removeEventListener('mousemove', manageMouseMove);
+    };
+  }, []);
 
+  // Return the JSX for your component
   return (
     <div className={styles.cursorContainer}>
       <motion.div 
