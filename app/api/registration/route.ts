a28b1a15faa817eca0f6cb7a3a003a48b5172df0
Muhammad Mubashir Munir Khan <100374421+mubashir05-beep@users.noreply.github.com>
2023-12-28 18:03:19 +0500

    Update route.ts

--- app/api/registration/route.ts
+++ app/api/registration/route.ts
@@ -36,6 +36,7 @@ export const POST = async (req: Request, res: Response) => {
       leaderName,
       aridNumber,
       phoneNumber,
+      updatedAt,
       email,
       section,
     } = body;
@@ -44,7 +45,7 @@ export const POST = async (req: Request, res: Response) => {
       where: {
         leaderName: leaderName,
         teamName: teamName,
-        email: email,
+        email:email
       },
     });
 
@@ -70,22 +71,45 @@ export const POST = async (req: Request, res: Response) => {
         section,
       },
     });
-
     const mailOptions: Mail.Options = {
       from: process.env.NEXT_PUBLIC_GMAIL,
-      to: [
-        "contact.muhammadmubashir@gmail.com",
-        "saifullah.akhtar13@gmail.com",
-      ],
-      subject: `Message from ${leaderName} (${email})`,
-      text: "",
+      to: email,
+      subject: `Thank You for Registering! Get Ready for InnoJam 2023!`,
+      html: `
+        <p>Dear ${leaderName},</p>
+    
+        <p>We are thrilled to inform you that your registration for the team ${teamName} in InnoJam 2023 has been successfully confirmed! We appreciate your enthusiasm for innovation and look forward to an exciting and collaborative experience with you.</p>
+        
+        <p><strong>Here are the details for InnoJam 2023:</strong></p>
+        
+        <p><strong>Registration Details:</strong></p>
+        
+        <ul>
+          <li>Team Leader: ${leaderName} - ${aridNumber} - ${section}</li>
+          <li>Date of Registration: ${updatedAt}</li>
+          <li>Team Name: ${teamName}</li>
+          <li>Team Members: ${teamMembers}</li>
+          <li>Email Address: ${email}</li>
+          <li>Phone Number: ${phoneNumber}</li>
+        </ul>
+    
+        <p>If the details you provided are incorrect or if you have any concerns, please don't hesitate to contact us at <a href="mailto:innojam40@gmail.com">innojam40@gmail.com</a> / <a href="tel:+923056207807">+92 305 6207807</a>. Your feedback is important to us, and we're here to ensure your InnoJam 2023 experience is seamless.</p>
+    
+        <p>If you have questions, need further details, or wish to contribute to make this event a success, we look forward to meeting you. Thank you for being part of InnoJam 2023, where your passion for innovation shapes the success of the event. We can't wait to witness the incredible projects that will emerge from this hackathon.</p>
+        
+        <p>Best Regards,</p>
+    
+        <p>Muhammad Mubashir Munir Khan,<br>Innojam 2023</p>
+      `,
     };
+
 
     const sendMailPromise = () =>
       new Promise<string>((resolve, reject) => {
         transport.sendMail(mailOptions, function (err) {
           if (!err) {
             resolve("Email sent");
+
           } else {
             reject(err.message);
           }
@@ -94,7 +118,7 @@ export const POST = async (req: Request, res: Response) => {
 
     console.log("New registration created:", newRegistration);
 
-    // await sendMailPromise();
+    await sendMailPromise();
 
     return NextResponse.json(
       { team: newRegistration, message: "User created successfully!" },
